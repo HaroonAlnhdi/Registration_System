360a2284b5394f895bd01136efc188e3cc3856dd
Haroon <raidsa511@gmail.com>
2023-05-12 22:32:20 +0300

    ini

---
+++ login.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+
+    <meta charset="utf-8">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="styles/loginStyle.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Saira Condensed">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+  </head>
+  <body>
+
+    <header>
+    <div class="nev">
+      <img src="img/lo.png" alt="logo" name="lo">
+      <h1>Registration System</h1>
+    </div>
+    </header>
+
+
+    <main class="">
+    <div class="conteiner">
+      <div class="mainrow">
+
+
+          <div class="picture">
+              <img class="pic" src="img/UOB.jpg" alt="">
+          </div>
+
+
+          <div class="loginpnal">
+
+
+
+              <div class="info">
+                <h1> Login Panel </h1>
+                  <p>Name*</p>
+                  <input type="text" class="ue" name="name" placeholder="Enter Your Name" />
+                  <p>Password*</p>
+                  <input type="password" class="ue" name="pass" placeholder="Enter Your Password" /><br>
+                  <a href="#">Forget Your Password?</a>
+                  <br />
+                  <input type="submit" class="SiginBytton" name="Sigin" value="Sign In">
+              </div>
+
+          </div>
+
+
+      </div>
+
+
+    </div>
+    </main>
+
+      <footer>
+
+  <p>COpyriht 2023 .Registration System. All Right Reserved</p>
+
+      </footer>
+
+  </body>
+</html>
